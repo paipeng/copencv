eca71f20d3e9a7e357a9c9c53e950bd950a19b82
Pai Peng <sipaipv6@gmail.com>
2021-11-04 11:22:00 +0800

    init MAC dylan project

---
+++ copencv_src/copencv.cpp
@@ -0,0 +1,34 @@
+//
+//  copencv.cpp
+//  copencv
+//
+//  Created by Pai Peng on 2021/11/4.
+//
+
+#include "copencv.hpp"
+#ifdef __cplusplus
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs/legacy/constants_c.h>
+#include <stdlib.h>
+
+
+#if DEBUG
+#if TARGET_OS_IPHONE
+#include <TargetConditionals.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#include <opencv2/imgcodecs/ios.h>
+#elif TARGET_OS_MAC
+//#import "NSImage+Mat.hpp"
+//#import "NSImage+CMatrix.h"
+#endif
+#endif
+
+#endif
+
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ copencv_src/copencv.hpp
@@ -0,0 +1,31 @@
+//
+//  copencv.hpp
+//  copencv
+//
+//  Created by Pai Peng on 2021/11/4.
+//
+
+#ifndef copencv_hpp
+#define copencv_hpp
+
+#ifdef __cplusplus
+#undef NO
+#undef YES
+#include <opencv2/opencv.hpp>
+#endif
+#include <stdio.h>
+
+
+using namespace std;
+
+#if defined(c_plusplus) || defined(__cplusplus)
+extern "C" {
+#endif
+
+int add(int a, int b);
+
+#if defined(c_plusplus) || defined(__cplusplus)
+}
+#endif
+
+#endif /* copencv_hpp */
